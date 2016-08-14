ecbbfa821de9e153a626e799c87534b3c6fdc033
crynobone <crynobone@gmail.com>
2016-08-14 21:27:16 +0800

    Improves docblock.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Collection.php
+++ src/Collection.php
@@ -6,22 +6,52 @@ use Money\Money;
 
 class Collection
 {
+    /**
+     * The Billplz client.
+     *
+     * @var \Billplz\Client
+     */
     protected $client;
 
+    /**
+     * Construct a new Collection.
+     *
+     * @param \Billplz\Client  $client
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * Create a new collection.
+     *
+     * @param  string  $title
+     * @param  array  $data
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function create($title, array $data = [])
     {
         return $this->client->send('POST', 'collections', [], array_merge(compact('title'), $data));
     }
 
+    /**
+     * Create a new open collection.
+     *
+     * @param  string  $title
+     * @param  string  $description
+     * @param  \Money\Money  $money
+     * @param  array  $data
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function createOpen($title, $description, Money $money, array $data = [])
     {
         $amount = $money->getAmount();
 
-        return $this->client->send('POST', 'open_collections', [], array_merge(compact('title', 'description', 'amount'), $data));
+        $body = array_merge(compact('title', 'description', 'amount'), $data);
+
+        return $this->client->send('POST', 'open_collections', [], $body);
     }
 }
