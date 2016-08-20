5382d19b1253bfbde2b68f38bfc76ffcd2f6be21
crynobone <crynobone@gmail.com>
2016-08-21 03:44:24 +0800

    Remove deprecated method.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Request.php
+++ src/Request.php
@@ -42,28 +42,11 @@ abstract class Request
      */
     protected function send($method, $path, array $headers = [], array $body = [])
     {
-        list($uri, $headers) = $this->endpoint($path, $headers);
-
-        return $this->client->send($method, $uri, $headers, $body);
-    }
-
-    /**
-     * Resolve API Endpoint URI and headers.
-     *
-     * @param  string  $path
-     * @param  array  $headers
-     *
-     * @return array
-     *
-     * @deprecated v0.4.2 To be removed in v0.5.0
-     */
-    protected function endpoint($path, array $headers = [])
-    {
         $domain = $this->client->getApiEndpoint();
 
         $uri = (new Uri(sprintf('%s/%s/%s', $domain, $this->version, $path)))
                     ->withUserInfo($this->client->getApiKey());
 
-        return [$uri, $headers];
+        return $this->client->send($method, $uri, $headers, $body);
     }
 }
