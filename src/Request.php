e1c20d19d3e0f2cc2556c0d4c493c851ff2bc51d
crynobone <crynobone@gmail.com>
2017-07-18 20:57:23 +0800

    Add support for both codex version.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Request.php
+++ src/Request.php
@@ -3,6 +3,7 @@
 namespace Billplz;
 
 use GuzzleHttp\Psr7\Uri;
+use Laravie\Codex\Endpoint;
 use Laravie\Codex\Request as BaseRequest;
 
 abstract class Request extends BaseRequest
@@ -19,5 +20,34 @@ abstract class Request extends BaseRequest
         $to = sprintf('%s/%s/%s', $this->client->getApiEndpoint(), $this->getVersion(), $endpoint);
 
         return (new Uri($to))->withUserInfo($this->client->getApiKey());
+    }
+
+    /**
+     * Get API Endpoint.
+     *
+     * @param  string|array  $path
+     *
+     * @return \Laravie\Codex\Endpoint
+     */
+    protected function getApiEndpoint($path = [])
+    {
+        $paths = (array) $path;
+
+        array_unshift($paths, $this->getVersion());
+
+        return parent::getApiEndpoint($paths);
+    }
+
+    /**
+     * Resolve URI.
+     *
+     * @param  \Laravie\Codex\Endpoint  $endpoint
+     *
+     * @return \GuzzleHttp\Psr7\Uri
+     */
+    protected function resolveUri(Endpoint $endpoint)
+    {
+        return parent::resolveUri($endpoint)
+                    ->withUserInfo($this->client->getApiKey());
     }
 }
