602956e38779d737618e088bfce66dbc383a0049
Mior Muhammad Zaki <crynobone@gmail.com>
2017-10-03 20:32:20 +0800

    Use php-cs-fixer v2.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/SanitizerTest.php
+++ tests/SanitizerTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class SanitizerTest extends TestCase
 {
     /** @test */
-    function it_has_proper_signature()
+    public function it_has_proper_signature()
     {
         $this->assertInstanceOf('Laravie\Codex\Sanitizer', new Sanitizer());
     }
