4113fe5d9bc5c9e96c2ebb99ecc65e0c23f2770e
Mior Muhammad Zaki <crynobone@gmail.com>
2017-10-30 22:05:23 +0800

    Fixes CS.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Three/Bank.php
+++ src/Three/Bank.php
@@ -1,22 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: khairul
- * Date: 29/10/2017
- * Time: 11:13 PM
- */
 
 namespace Billplz\Three;
 
 class Bank extends Request
 {
     /**
-     * Get list of bank for Bank Direct Feature
+     * Get list of bank for Bank Direct Feature.
      *
      * @return \Laravie\Codex\Response
      */
     public function supportedForFpx()
     {
-        return $this->send('GET', "fpx_banks");
+        return $this->send('GET', 'fpx_banks');
     }
 }
