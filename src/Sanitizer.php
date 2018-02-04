d95bea700f6a8d3596de502cb0fc3ad59e60fe6b
Mior Muhammad Zaki <crynobone@gmail.com>
2018-02-04 19:01:45 +0800

    Accept Sanitizer cast override.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Sanitizer.php
+++ src/Sanitizer.php
@@ -8,10 +8,12 @@ class Sanitizer extends BaseSanitizer
 {
     /**
      * Construct a new sanitizer.
+     *
+     * @param  array  $casts
      */
-    public function __construct()
+    public function __construct(array $casts = [])
     {
-        $this->casts = [
+        $this->casts = array_merge($casts, [
             'total' => new Casts\Money(),
             'amount' => new Casts\Money(),
             'due_at' => new Casts\DateTime(),
@@ -20,6 +22,6 @@ class Sanitizer extends BaseSanitizer
             'split_payment' => [
                 'fixed_cut' => new Casts\Money(),
             ],
-        ];
+        ]);
     }
 }
