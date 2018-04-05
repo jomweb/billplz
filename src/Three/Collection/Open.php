ee5836733fc2395008402e1ab3d79690668d04c1
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-05 12:24:13 +0800

    Add request for open collection.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Three/Collection/Open.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Billplz\Three\Collection;
+
+use Billplz\Base\Collection\Open as Request;
+
+class Open extends Request
+{
+    /**
+     * Version namespace.
+     *
+     * @var string
+     */
+    protected $version = 'v4';
+}
