eaf4828ba75455f4ade95a12aad7782da7cca613
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-10 17:26:21 +0800

    Add additional tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Three/Collection/PaymentMethodTest.php
+++ tests/Three/Collection/PaymentMethodTest.php
@@ -12,4 +12,14 @@ class PaymentMethodTest extends PaymentMethodTestCase
      * @var string
      */
     protected $apiVersion = 'v3';
+
+
+    /** @test */
+    public function it_resolve_the_correct_version()
+    {
+        $payment = $this->makeClient()->uses('Collection.PaymentMethod', 'v3');
+
+        $this->assertInstanceOf('Billplz\Three\Collection\PaymentMethod', $payment);
+        $this->assertSame('v3', $payment->getVersion());
+    }
 }
