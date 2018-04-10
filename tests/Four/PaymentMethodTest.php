cd1773b4afead08b8a1cff83a19dabf355449bb6
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-10 14:44:32 +0800

    Added Get Payment Method Index Test

---
+++ tests/Four/PaymentMethodTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Billplz\TestCase\Four;
+
+use Billplz\TestCase\TestCase;
+use \Laravie\Codex\Contracts\Response;
+
+class PaymentMethod extends TestCase
+{
+    /**
+     * API Version.
+     *
+     * @var string
+     */
+    protected $apiVersion = 'v4';
+    
+    /** @test */
+    public function it_can_get_payment_methods()
+    {
+        $expected = '{"payment_methods":[{"code": "paypal","name": "PAYPAL","active": true},{"code": "fpx","name": "Online Banking","active": false}]}';
+
+        $faker = $this->expectRequest('GET', 'collections/0idsxnh5/payment_methods')
+            ->shouldResponseWith(200, $expected);
+        
+        $response = $this->makeClient($faker)
+                        ->uses('PaymentMethod')
+                        ->show('0idsxnh5');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame($expected, $response->getBody());
+    }
+}
