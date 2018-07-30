e74a89e0d78313eb4e27caf5d8487fd70f07d914
Mior Muhammad Zaki <crynobone@gmail.com>
2018-07-31 06:55:46 +0800

    Add webhook rank.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Four/Webhook.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Billplz\Four;
+
+use Billplz\Request;
+use Laravie\Codex\Contracts\Response;
+
+class Webhook extends Request
+{
+    /**
+     * Version namespace.
+     *
+     * @var string
+     */
+    protected $version = 'v4';
+
+    /**
+     * Get webhook rank for the account.
+     *
+     * @return \Laravie\Codex\Contracts\Response
+     */
+    public function rank(): Response
+    {
+        return $this->send('GET', 'webhook_rank', [], []);
+    }
+}
---
+++ tests/Four/WebhookTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Billplz\TestCase\Four;
+
+use Billplz\TestCase\TestCase;
+use Laravie\Codex\Contracts\Response;
+
+class WebhookTest extends TestCase
+{
+    /**
+     * API Version.
+     *
+     * @var string
+     */
+    protected $apiVersion = 'v4';
+
+    /** @test */
+    public function it_resolve_the_correct_version()
+    {
+        $payment = $this->makeClient()->uses('Webhook', 'v4');
+
+        $this->assertInstanceOf('Billplz\Four\Webhook', $payment);
+        $this->assertSame('v4', $payment->getVersion());
+    }
+
+    /** @test */
+    public function it_can_get_webhook_rank()
+    {
+        $expected = '{"rank":1.2}';
+
+        $faker = $this->expectRequest('GET', 'webhook_rank')
+                    ->shouldResponseWith(200, $expected);
+
+        $response = $this->makeClient($faker)->uses('Webhook')->rank();
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame($expected, $response->getBody());
+    }
+}
