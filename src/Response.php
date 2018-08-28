b2f55f2c587270d2a542400ebd0fdddb135f6667
Mior Muhammad Zaki <crynobone@gmail.com>
2018-08-28 21:20:44 +0800

    Add custom validation.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Response.php
+++ src/Response.php
@@ -6,5 +6,16 @@ use Laravie\Codex\Response as BaseResponse;
 
 class Response extends BaseResponse
 {
-    //
+    /**
+     * Validate the response object.
+     *
+     * @return $this
+     */
+    public function validate()
+    {
+        $this->abortIfRequestUnauthorized();
+        $this->abortIfRequestHasFailed();
+
+        return $this;
+    }
 }
