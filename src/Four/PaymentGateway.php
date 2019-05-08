a447d26dd14c7ecc3049ed87128c51bec5423d97
Mior Muhammad Zaki <crynobone@gmail.com>
2019-05-09 07:56:13 +0800

    Add Payment Gateway API.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Four/PaymentGateway.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Billplz\Four;
+
+use Billplz\Request;
+use Laravie\Codex\Contracts\Response;
+
+class PaymentGateway extends Request
+{
+    /**
+     * Version namespace.
+     *
+     * @var string
+     */
+    protected $version = 'v4';
+
+     /**
+     * Get payment gateways index.
+     *
+     * @return \Billplz\Response
+     */
+    public function all(): Response
+    {
+        return $this->send('GET', 'payment_gateways', [], []);
+    }
+}
